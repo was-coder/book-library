de12bf6c9465254fa7ee5a78981cfd12d790331f
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-12-15 22:43:33 +0100

    fix:Made the popup more responsive and neat

--- src/js/functionality/newBookAdded.js
+++ src/js/functionality/newBookAdded.js
@@ -5,12 +5,13 @@ class NewBookAdded {
     successSection.className = 'success-container';
     successSection.innerHTML = `
       <div class = "success-background"
-      <div class = "success">
-      <h3 id="success-header">New Book Added Successfully</h3>
-      <p class = "success-author" >${author}</p>
-      <p>${title} has been added successfully</p>
-      <button class="list-button button" id="list" data-target="bookList">Return to Book List</button>
-      <button class="addNewBook-button button">Add More Book</button>
+      <div>
+      <h3 id="success-header">New Book Added</h3>
+      <p class="successful">The Book titled: "${title} by ${author}" has been added successfully</p>
+      <div class="success-btn">
+      <button class="list-button button  pop-btn" id="list" data-target="bookList">Return to Book List</button>
+      <button class="addNewBook-button button pop-btn">Add More Book</button>
+      </div>
       </div>
       </div>
       `;
